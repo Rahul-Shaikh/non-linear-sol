4e522ab257a0fa846fb3ca35afc6644b01dd7c4a
rahul5720 <rahulshaikh5720.ac@gmail.com>
2018-12-26 14:35:53 +0530

    added bisection method

---
+++ non-linear-sol.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <math.h>
+
+#define err 0.00000001
+
+double dabs(double x)
+{
+    if (x>0) return x;
+    else return -x;
+}
+double f(double x)
+{
+    return pow(x,3) - 2*pow(x,2) + 1;
+}
+
+void bisection();
+
+int main(int argc, char const *argv[])
+{
+    printf("1. Enter 'B' to solve using bisection method\n");
+    printf("\nEnter choice : ");
+    char c;
+    scanf("%c", &c);
+    if (c == 'B') bisection();
+    else printf("Wrong input\n");
+    return 0;
+}
+
+void bisection()
+{
+    int count = 0;
+    double x1, x2, xmid;
+    printf("\nEnter the two intial guesses : ");
+    scanf("%lf%lf", &x1, &x2);
+    printf("\nDo you want to see each step?[y/n] : \n");
+    char cm;
+    while (1)
+    {
+        while (getchar() != '\n');
+        scanf("%c", &cm);
+        if ((cm=='y')||(cm=='n')) break;
+        else printf("Wrong input\n");
+        printf("\n");
+    }
+    while (count<500)
+    {
+        xmid = (x1 + x2)/2;
+        count++;
+        if (dabs(f(xmid)) < err)
+        {
+            printf("The root is found at iteration no. %-3d and the root is %10lf\n\n", count, xmid);
+            return;
+        }
+        if (cm == 'y') printf("At iteration no. %-3d the value of x is %13.8lf and f(x) is %13.8lf\n", count, xmid, f(xmid));
+        if (f(x1)*f(xmid) > 0) x1 = xmid;
+        else x2 = xmid;
+    }
+    printf("The function does not converge with the given intial guesses in 500 iterations\n\n");
+}
